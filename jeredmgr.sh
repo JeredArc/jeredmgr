f99519779603bef023302b716d70a2394ec86f1e
Jered <sh@git-123.heferl.net>
2025-05-13 01:42:45 +0200

    Fixing pre-commit hook version bumping

--- jeredmgr.sh
+++ jeredmgr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ####################################################################
-# JeredMgr 1.0.6                                                   #
+# JeredMgr 1.0.7                                                   #
 # A tool that helps you install, run, and update multiple projects #
 # using Docker containers, systemd services, or custom scripts.    #
 ####################################################################
