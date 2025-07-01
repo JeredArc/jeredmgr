dcb273ec4fce660fe843cfa37e9b1008d8fe3887
Jered <sh@git-123.heferl.net>
2025-07-01 17:52:03 +0200

    Small fixes.

--- jeredmgr.sh
+++ jeredmgr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ####################################################################
-# JeredMgr 1.0.27                                                  #
+# JeredMgr 1.0.28                                                  #
 # A tool that helps you install, run, and update multiple projects #
 # using Docker containers, systemd services, or custom scripts.    #
 ####################################################################
@@ -1401,7 +1401,7 @@ shell_project() {  # args: $project_name, reads: $enabled $type $path $project_n
 		fi
 	fi
 
-	echo "Opening shell in container for service '$service_name' ..."
+	format_header "Opening shell for project $(format_project "$project_name") in container for service ${BOLD}$service_name${RESET} ..."
 	docker compose -f "$compose_file" --project-directory "$path" exec "$service_name" sh -l
 }
 
